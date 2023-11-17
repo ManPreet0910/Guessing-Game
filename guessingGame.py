2acc9b76957a7acf3290aa249a3213cf5f74c384
ManPreet0910 <133527039+ManPreet0910@users.noreply.github.com>
2023-11-16 20:36:24 -0500

    Add files via upload

--- guessingGame.py
+++ guessingGame.py
@@ -2,30 +2,16 @@ import random
 
 chance = 5
 number = (random.randint(1,9))
-guess = ""
+
 
 while chance > 0:
-    guess = (input("enter your guess:"))
+    guess = int(input("enter your guess:"))
     if guess == number:
         print("congrats you got the number correct")
     elif guess != number:
         chance - 1
-        print("you lost")
+        print("you guessed worng, try again")
+        break
 
 if chance == 0:
-        print("your lose the number was", number)
-
-    
-
-
-# if guess == number:
-#         print("Congrats YOU WON!")
-# elif guess != number:
-#         chance = chance - 1
-#         print("Your guess was incorect try again, you have", str(chance),"more chance left")
-
-# while chance < 5 :
-#     if chance == 0:
-#         print("YOU LOSE!!! the number is", number)
-        
-        
+    print("you lose")
